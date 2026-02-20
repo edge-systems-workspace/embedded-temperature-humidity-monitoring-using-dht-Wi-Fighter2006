e6b6495053252b680d4ff9c4a6abfd5257db3d18
Wi-Fighter2006 <piyushchudhary97@gmail.com>
2026-02-20 10:58:20 +0530

    Create DHT Object + Initialize Serial

--- src/main.cpp
+++ src/main.cpp
@@ -5,21 +5,20 @@
  * @brief Embedded Temperature and Humidity Monitoring using DHT11
  * @author Piyush Choudhary
  * @date 2026-02-20
- *
- * @details
- * Reads environmental data from a DHT11 sensor and outputs
- * temperature and humidity values to the Serial Monitor.
  */
 
 #include <DHT.h>
 
-// STEP 1: Define the DHT data pin
 #define DHT_PIN 2
-
-// STEP 1: Define the DHT sensor type
 #define DHT_TYPE DHT11
 
+// STEP 2: Create DHT object
+DHT dht(DHT_PIN, DHT_TYPE);
+
 void setup() {
+
+    // Initialize Serial communication
+    Serial.begin(9600);
 }
 
 void loop() {
